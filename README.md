7cf06edee595aded2f429e510484ac49c9ea8aa4
Lion Chen <lion@lion-mac.internal.synopsys.com>
2018-07-27 13:21:00 -0600

    clean up

--- README.md
+++
@@ -1,1 +0,0 @@
-# shift-scheduler-deployment
--- lib/create_service_account_cred.sh
+++
@@ -1,7 +0,0 @@
-export GOOGLE_APPLICATION_CREDENTIALS=~/.config/gcloud/terraform-admin.json
-
-gcloud auth login
-
-export TF_VAR_project_id="$(gcloud projects list  --format="csv[no-heading](PROJECT_ID)")"
-
-gcloud iam service-accounts keys create "${GOOGLE_APPLICATION_CREDENTIALS}" --iam-account terraform@"${TF_VAR_project_id}".iam.gserviceaccount.com
