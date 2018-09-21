a51aa1aea9bc4fb1c3d770bdfccbe8648f676a69
lion <>
2018-09-20 19:41:18 -0600

    Revert "Changed bucket name as a result of an auto created bucket."
    This reverts commit 96e328860c60caf1e8c780168fe604952acf8b54.

---
+++ cloud-project/service-account/service_account.tf
@@ -0,0 +1,68 @@
+variable "project_id" {
+  type = "string"
+}
+variable "service_account" {
+  type = "string"
+}
+
+terraform {
+  backend "gcs" {
+    bucket  = "shift-scheduler-00000-terraform-state"
+  }
+}
+
+provider "google" {}
+
+resource "google_service_account" "ci_account" {
+  account_id = "${var.service_account}"
+  project = "${var.project_id}"
+}
+
+module "ci_account_k8s_deployer" {
+  source = "./account-role"
+
+  role = "roles/container.developer"
+  id = "${google_service_account.ci_account.unique_id}"
+  email = "${google_service_account.ci_account.email}"
+}
+
+module "ci_account_ip_viewer" {
+  source = "./account-role"
+
+  role = "roles/compute.viewer"
+  id = "${google_service_account.ci_account.unique_id}"
+  email = "${google_service_account.ci_account.email}"
+}
+
+resource "google_service_account_key" "ci_deploy_key" {
+  service_account_id = "${google_service_account.ci_account.unique_id}"
+
+  provisioner "local-exec" {
+    working_dir = "../../../shift-scheduler-deployment"
+    environment {
+      KEY = "${base64decode(google_service_account_key.ci_deploy_key.private_key)}"
+    }
+
+    command = <<EOF
+      echo ${KEY} >> ${TF_VAR_service_account}-key.json
+      travis login --org
+      travis encrypt-file ${TF_VAR_service_account}-key.json --add
+      rm ${TF_VAR_service_account}-key.json
+      git add ${TF_VAR_service_account}-key.json.enc
+      git commit -m "Changed deploy service account."
+      git push
+    EOF
+  }
+
+  provisioner "local-exec" {
+    when = "destroy"
+    working_dir = "../../../shift-scheduler-deployment"
+
+    command = <<EOF
+      rm ${TF_VAR_service_account}-key.json.enc
+      git add ${TF_VAR_service_account}-key.json.enc
+      git commit -m "Deleted deploy service account."
+      git push
+    EOF
+  }
+}
