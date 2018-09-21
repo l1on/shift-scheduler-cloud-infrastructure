c910322c4a732368da3a5cf0b87640d58d5df85d
lion <>
2018-09-20 22:01:46 -0600

    auto approvoal

--- lib/tf-apply.sh
+++ lib/tf-apply.sh
@@ -3,5 +3,5 @@ export TF_VAR_project_id="$(gcloud projects list --format="csv[no-heading](PROJE
 gcloud auth revoke -q
 
 gcloud auth application-default login
-terraform init && terraform apply
+terraform init && terraform apply -auto-approve
 gcloud auth application-default revoke -q
--- lib/tf-destroy.sh
+++ lib/tf-destroy.sh
@@ -3,5 +3,5 @@ export TF_VAR_project_id="$(gcloud projects list  --format="csv[no-heading](PROJ
 gcloud auth revoke -q
 
 gcloud auth application-default login
-terraform init && terraform destroy
+terraform init && terraform destroy 
 gcloud auth application-default revoke -q
